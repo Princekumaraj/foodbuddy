6f60f5eef57c9b1d083c0413d8c0c356fd628ecf
Prince Kumar Raj <66669293+Princekumaraj@users.noreply.github.com>
2022-04-11 02:03:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
 document.querySelector(".menu").addEventListener("click",()=>{document.querySelectorAll(".target").forEach(e=>{e.classList.toggle("change")})});const icons=document.querySelectorAll(".section-1-icons i");let i=1;setInterval(()=>{i++;const e=document.querySelector(".section-1-icons .change");e.classList.remove("change"),i>icons.length?(icons[0].classList.add("change"),i=1):e.nextElementSibling.classList.add("change")},4e3);
 
-
-document.addEventListener("contextmenu",event => event.preventDefault()};
-
-documentr.onkeydown = function(e) {
-	
-	if(e.keyCode == 123) {
-		alert("F12 disable");
-		return false;
-	}
-	if(e.ctrlkey && e.keyCode == 67){
-		alert(("ctrl + c disable");
-		return false;
-	}
-	
-	if(e.ctrlkey && e.keyCode == 85){
-		alert("ctrl + u disable");
-		return false;
-	}
-}
-
